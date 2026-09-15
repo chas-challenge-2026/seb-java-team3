3f1ec8c2cdbbb5119b7db0c798b1a86d4ac1b757
Jonathan Isaksson <tignaltus@gmail.com>
2026-09-15 21:11:58 +0200

    Frontend fix for JWT Implementation
    Frontenden hade inte kopplats till JWT så JWT ändringarna skulle få frontenden sluta fungera. Dessa ändringar ska förhoppningsvis se till att de fungerar efter ändringarna.

--- frontend/src/features/auth/useLogin.ts
+++ frontend/src/features/auth/useLogin.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
 import { fetchLoginUser } from "./api";
+import { setToken } from "../../lib/authToken";
 
 export function useLogin() {
   const queryClient = useQueryClient();
@@ -9,6 +10,7 @@ export function useLogin() {
   return useMutation({
     mutationFn: fetchLoginUser,
     onSuccess: (user) => {
+      setToken(user.token);
       queryClient.setQueryData(["auth", "me"], user);
       navigate({ to: "/" });
     },
